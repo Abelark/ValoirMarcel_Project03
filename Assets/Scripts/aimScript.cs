5314d8083d7fe62aa5e3ffc8f09ec6871052124f
Marcel Valoir <myv180000@utdallas.edu>
2021-12-03 19:22:34 -0600

    Created Right Mouse ADS
    Learned the new UnityEngine.InputSystem + Cinemachine to implement this.

---
+++ Assets/Scripts/aimScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
+
+public class aimScript : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    [SerializeField] PlayerInput playerInput;
+    [SerializeField] InputActionAsset inputActions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //On
+    }
+}
